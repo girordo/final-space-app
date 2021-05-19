82e1c9b8ec0e93dc0b2923a6a9c7741e5696b858
Tarcisio Giroldo <tarcisio.siqueira@usp.br>
2021-05-19 19:39:41 -0300

    (feat): created header centered

--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full h-24 border-b">
+    <div className="w-full h-24 border-b flex justify-center items-center">
       <h4>Header da aplicaçao queria que fosse sticky</h4>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Final Space App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
