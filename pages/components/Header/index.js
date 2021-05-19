0d4f592aa35170d9e6aed4d75d09084c81f39345
Tarcisio Giroldo <tarcisio.siqueira@usp.br>
2021-05-19 19:33:54 -0300

    (feat): created header and footer

---
+++ pages/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full h-24 border-b">
+      <h4>Header da aplicaçao queria que fosse sticky</h4>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
 import Head from "next/head";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{" "}
+          Welcome to
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{" "}
+          Get started by editing
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
             pages/index.js
           </code>
@@ -61,17 +63,9 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
-}
+};
+
+export default Home;
