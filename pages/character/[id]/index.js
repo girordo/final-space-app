0de6a205b926b871b9bec53d53237d076d5d0226
girordo <tsgiroldo@gmail.com>
2021-05-20 22:36:15 -0300

    feat: drag animation on character card

--- pages/character/[id]/index.js
+++ pages/character/[id]/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
@@ -38,13 +39,34 @@ const Character = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <img
-          src={characterData.img_url}
-          className="rounded-2xl"
-          alt="Character Final Space"
-        />
-        <h1>{characterData.name}</h1>
+      <main className="flex flex-col items-center justify-center w-full min-h-screen flex-1 px-20 text-center">
+        <motion.section
+          drag
+          dragElastic={0.2}
+          className="bg-gray-200  p-4 flex flex-col shadow-lg rounded-lg">
+          <img
+            src={characterData.img_url}
+            className="rounded-full mr-4"
+            alt="Character Final Space"
+          />
+          <div className="flex flex-col justify-center hover:text-secondary-800 transition-colors">
+            <ul className="text-left">
+              <li className="mb-4">
+                <h1 className="text-2xl font-bold">{characterData.name}</h1>
+              </li>
+              <li className="flex flex-row mb-0.5 text-sm">
+                Status:<h4 className="font-semibold">{characterData.status}</h4>
+              </li>
+              <li className="flex flex-row mb-0.5 text-sm">
+                Gender:<h4 className="font-semibold">{characterData.gender}</h4>
+              </li>
+              <li className="flex flex-row text-sm">
+                Species:
+                <h4 className="font-semibold">{characterData.species}</h4>
+              </li>
+            </ul>
+          </div>
+        </motion.section>
       </main>
       <Footer />
     </div>
