09c679bbd56f2ef95ac1f03ad5408603918ee35f
Tarcisio Giroldo <tarcisio.siqueira@usp.br>
2021-05-24 21:09:15 -0300

    (feat) locationData was wrong

--- pages/locations/[id]/index.js
+++ pages/locations/[id]/index.js
@@ -23,7 +23,7 @@ export async function getStaticProps({ params }) {
   const data = await res.json();
   return {
     props: {
-      locationDaat: data,
+      locationData: data,
     },
   };
 }
