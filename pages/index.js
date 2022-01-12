8038e845cdd8d693084e0d6a8ceb012275dc0fa2
Tarcísio Giroldo Siqueira <tsgiroldo@gmail.com>
2022-01-12 16:45:03 -0300

    :fire: | Removing motion

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from "framer-motion";
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
