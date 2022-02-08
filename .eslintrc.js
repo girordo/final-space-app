75f0381801791578cb6217af314403692ada2be2
Tarcísio Giroldo <tsgiroldo@gmail.com>
2022-02-08 20:02:42 -0300

    :fire: | Idk a lot of changes and config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,16 +17,30 @@ module.exports = {
     amd: true,
     node: true,
   },
+  plugins: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
+    "simple-import-sort",
+    "prettier",
+    "plugin:tailwindcss/recommended",
   ],
   rules: {
     "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-    "no-unused-vars": "off",
+    "no-unused-vars": "warn",
     "react/react-in-jsx-scope": "off",
+    "jsx-a11y/accessible-emoji": "off",
+    "react/prop-types": "off",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
@@ -35,6 +49,9 @@ module.exports = {
         aspects: ["invalidHref", "preferButton"],
       },
     ],
-    "jsx-a11y/accessible-emoji": "off",
+    "jsx-a11y/anchor-has-content": "off",
+    "tailwindcss/classnames-order": "warn",
+    "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/no-contradicting-classname": "error",
   },
 };
---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Lint then format TypeScript and JavaScript files
+  "**/*.(ts|tsx|js|jsx)": (filenames) => [
+    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": (filenames) =>
+    `yarn prettier --write ${filenames.join(" ")}`,
+};
