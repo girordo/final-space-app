78074c7fef1714d91b2e3ef93f96d6cadee720b2
Tarcísio Giroldo <tsgiroldo@gmail.com>
2022-07-17 02:47:04 -0300

    :art: | Improving something

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default NotFoundPage = () => {
   return (
--- test/header.test.js
+++ test/header.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 afterEach(cleanup);
 
